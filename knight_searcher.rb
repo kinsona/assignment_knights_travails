4ee2b5f953d1f1f15863fb2c73e290330dcfef81
Adam <adam.kinson@gmail.com>
2015-06-14 10:13:08 -0400

    finish DFS method; bugfix needed for backtracking movement

--- knight_searcher.rb
+++ knight_searcher.rb
@@ -5,6 +5,7 @@ class KnightSearcher
   def initialize(tree)
     @tree = tree
     @queue = []
+    @stack = []
   end
 
 
@@ -39,11 +40,48 @@ class KnightSearcher
   end
 
 
+  def dfs_for(target_coords)
+
+    failed = true
+
+    @stack << @tree.root
+
+    until @stack.empty? do
+
+      square = @stack.pop
+      #puts square.x.to_s + square.y.to_s
+
+      square.depth ||= []
+      square.depth << [square.x, square.y]
+
+      if match?(square, target_coords)
+        failed = false
+        match_found(square)
+        break
+
+      elsif !square.children.nil?
+        stack_children(square)
+
+      end
+
+    end
+
+    clear_dfs
+    puts "Search complete: no matches found." if failed
+  end
+
+
   private
 
 
   def clear_bfs
     @queue = []
+    @tree.clear_all_depths
+  end
+
+
+  def clear_dfs
+    @stack = []
     @tree.clear_all_depths
   end
 
@@ -54,7 +92,7 @@ class KnightSearcher
 
 
   def match_found(square)
-    puts "Found in #{square.depth.size} moves:"
+    puts "#{square.depth.size} move(s):"
     square.depth.each { |step| print "#{step}\n"}
   end
 
@@ -63,7 +101,21 @@ class KnightSearcher
     square.children.each do |child|
       if child.depth.nil?
         child.depth = square.depth.dup
+        print child.depth.to_s + ">> #{child.x},#{child.y}\n"
         @queue << child
+      end
+    end
+  end
+
+
+  def stack_children(square)
+    # sort in a more preferable order?
+    # Check to see why 5,4 keeps showing up?
+    square.children.each do |child|
+      if child.depth.nil?
+        child.depth = square.depth.dup
+        print child.depth.to_s + ">> #{child.x},#{child.y}\n"
+        @stack << child
       end
     end
   end
